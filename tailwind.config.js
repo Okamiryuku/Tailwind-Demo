22c126e53736aba1faffff8d88b333bffbcd72b3
Okamiryuku <okamiryuku@gmail.com>
2024-01-08 22:29:35 -0600

    Style Card Element

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", 
-    "./public/index.html"
+    './src/*.{html,js}',
+    './public/*.{html,js}'
   ],
   theme: {
     extend: {
