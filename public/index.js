992002cfcaca9390c5d1cc60b81c774afcf3fd9c
Okamiryuku <okamiryuku@gmail.com>
2024-01-12 19:19:32 -0600

    Added JS fuc for menu styling

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
 
 burger.addEventListener('click', () => {
     if(menu.classList.contains('hidden')) {
@@ -8,3 +11,21 @@ burger.addEventListener('click', () => {
         menu.classList.add('hidden');
     }
 });
+
+if(document.URL.includes('index.html')) {
+    document.querySelector('#home-li').classList.add('font-bold')
+    home.classList.remove('border-white')
+    home.classList.add('border-primary')
+}
+
+if(document.URL.includes('about.html')) {
+    document.querySelector('#about-li').classList.add('font-bold')
+    about.classList.remove('border-white')
+    about.classList.add('border-primary')
+}
+
+if(document.URL.includes('contact.html')) {
+    document.querySelector('#contact-li').classList.add('font-bold')
+    contact.classList.remove('border-white')
+    contact.classList.add('border-primary')
+}
